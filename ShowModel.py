46583bac43e3b57068f98eac6280ab305dd1e64b
wangshiphys <wangshiphys@gmail.com>
2019-01-11 14:38:20 +0800

    Modified the `ShowModel` module Redesign this module to show all bonds between nearest-neighbor David-Stars and classify these bonds according to their length

--- ShowModel.py
+++ ShowModel.py
@@ -6,79 +6,50 @@ An illustration of our David-Star tight-binding model
 import matplotlib.pyplot as plt
 import numpy as np
 
-from TaS2DataBase import  As_STAR, COORDS_STAR
+from TaS2DataBase import  As_STAR, COORDS_STAR, ALL_BONDS
 
 
-POINT_TYPE_A = (0, )
-POINT_TYPE_B = (1, 2, 3, 4, 5, 6)
-POINT_TYPE_C = (7, 8, 9, 10, 11, 12)
-
-
+line_width = 2
+marker_size = 25
+font_size = "xx-large"
 color_map = plt.get_cmap("tab10")
 colors = color_map(range(color_map.N))
 
-alpha = 0.5
-line_width = 3
-marker_size = 26
-line_style = "dashed"
-font_size = "xx-large"
-hopping = r"$C_{i\sigma}^{\dag}C_{j\sigma}$"
-legends = ["$t_{0}$".format(i) + hopping for i in range(5)]
+shrink_inner, shrink_outer = 0.2, 0.1
+# shrink_inner, shrink_outer = 0.00, 0.00
+# shrink_inner, shrink_outer = 0.064, 0.042783
 
-shrink_inner = 0.3
-shrink_outer = 0.2
 coords_star = np.array(COORDS_STAR, copy=True)
 coords_star[1:7, :] *= (1 - shrink_inner)
 coords_star[7:, :] *= (1 - shrink_outer)
 
+legend_handles = []
+hopping = r"$C_{i\sigma}^{\dag}C_{j\sigma}$"
+legend_labels = ["$t_{0}$".format(i) + hopping for i in range(6)]
+line_styles = ["dashed", "dashed", "dashed", "dotted", "dotted", "dotted"]
+log_template = "Bond: (p0=(({0:>2d}, {1:>2d}), {2:>2d}), " \
+               "p1=(({3:>2d}, {4:>2d}), {5:>2d})), Length = {6:.8f}"
 
 fig, ax = plt.subplots()
-# Bonds between the central point and the points of the inner loop in the
-# David-Star
-for p0 in POINT_TYPE_A:
-    for p1 in POINT_TYPE_B:
-        bond = coords_star[[p0, p1]]
-        bond_type0, = ax.plot(
-            bond[:, 0], bond[:, 1],
-            color=colors[0], lw=line_width, ls=line_style, alpha=alpha,
+for bonds, line_style, color in zip(ALL_BONDS, line_styles, colors):
+    for (v0, index0), (v1, index1) in bonds:
+        p0 = np.dot(v0, As_STAR) + coords_star[index0]
+        p1 = np.dot(v1, As_STAR) + coords_star[index1]
+        length = np.linalg.norm(p0 - p1)
+        line, = ax.plot(
+            (p0[0], p1[0]), (p0[1], p1[1]),
+            color=color, lw=line_width, ls=line_style
         )
-
-# Bonds between the inner loop in the David-Star
-bond = coords_star[[1, 2, 3, 4, 5, 6, 1]]
-bond_type1, = ax.plot(
-    bond[:, 0], bond[:, 1],
-    color=colors[1], lw=line_width, ls=line_style, alpha=alpha,
-)
-
-# Bonds between the inner loop and the outer loop in the David-Star
-bond = coords_star[[1, 7, 2, 8, 3, 9, 4, 10, 5, 11, 6, 12, 1]]
-bond_type2, = ax.plot(
-    bond[:, 0], bond[:, 1],
-    color=colors[2], lw=line_width, ls=line_style, alpha=alpha,
-)
-
-# Bonds between points from one David-Star's outer loop and another
-# David-Star's outer loop
-bond = np.array([coords_star[9], coords_star[12] + As_STAR[0]])
-bond_type3, = ax.plot(
-    bond[:, 0], bond[:, 1],
-    color=colors[3], lw=line_width, ls=line_style, alpha=alpha,
-)
-
-# Bonds between points from one David-Star's inner loop and another
-# David-Star's outer loop
-bond = np.array([coords_star[3], coords_star[12] + As_STAR[0]])
-bond_type4, = ax.plot(
-    bond[:, 0], bond[:, 1],
-    color=colors[4], lw=line_width, ls=line_style, alpha=alpha,
-)
+        print(log_template.format(*v0, index0, *v1, index1, length))
+    print("=" * 80, flush=True)
+    legend_handles.append(line)
 ax.legend(
-    [bond_type0, bond_type1, bond_type2, bond_type3, bond_type4],
-    legends, loc="upper left", fontsize=font_size,
+    legend_handles, legend_labels, loc="upper right", fontsize="x-large",
 )
 
-# Show the three type points
-for points in [coords_star, coords_star + As_STAR[0]]:
+# Annotate the points with their indices
+for translation in [(0, 0), (1, 0), (0, 1), (-1, 1)]:
+    points = coords_star + np.dot(translation, As_STAR)
     ax.plot(
         points[0, 0], points[0, 1],
         color=colors[3], marker="o", ls="", ms=marker_size,
@@ -91,15 +62,15 @@ for points in [coords_star, coords_star + As_STAR[0]]:
         points[7:, 0], points[7:, 1],
         color=colors[2], marker="o", ls="", ms=marker_size,
     )
+    for i in range(13):
+        ax.annotate(
+            "{0}".format(i), (points[i, 0], points[i, 1]),
+            ha="center", va="center", fontsize=font_size,
+        )
 
-# Annotate the points with their indices
-for i in range(13):
-    ax.annotate(
-        "{0}".format(i), (coords_star[i, 0], coords_star[i, 1]),
-        ha="center", va="center", fontsize=font_size,
-    )
-
-ax.set_title("David-Star Tight-Binding Model", fontsize=font_size)
+ax.set_title(
+    "Demonstration of David-Star Tight-Binding Model", fontsize=font_size
+)
 ax.set_aspect("equal")
 ax.set_axis_off()
 plt.show()
