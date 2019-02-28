20250592c6879b815f80132c4b002b0d7866281e
wangshiphys <wangshiphys@gmail.com>
2019-02-28 13:53:17 +0800

    Some of the matplotlib parameters such as line_width, marker_size, font_size, etc., are now defined as project level constant, and imported to this module when needed.

--- ShowCommensurate.py
+++ ShowCommensurate.py
@@ -7,9 +7,9 @@ system
 import matplotlib.pyplot as plt
 import numpy as np
 
+from TaS2DataBase import LINE_WIDTH, MARKER_SIZE, FONT_SIZE, FIG_DPI
+from TaS2DataBase import SPINE_WIDTH, TICK_WIDTH, TICK_LENGTH, LABEL_SIZE
 
-line_width = 5
-marker_size = 10
 
 start = 0
 end = 8
@@ -23,11 +23,19 @@ ys2 = np.cos(2 * np.pi * alphas / np.sqrt(3))
 fig, axes = plt.subplots(3, 1, sharex=True)
 titles = [r"$R_2=1$", r"$R_2=3/2$", r"$R_2=\sqrt{3}$"]
 for ax, ys, title in zip(axes, [ys0, ys1, ys2], titles):
-    ax.plot(alphas, ys, lw=line_width)
-    ax.plot(xs, [0]*len(xs), marker="o", ls="", ms=marker_size)
+    ax.plot(alphas, ys, lw=LINE_WIDTH)
+    ax.plot(xs, [0] * len(xs), marker="o", ls="", ms=MARKER_SIZE)
     ax.set_xlim(start, end)
-    ax.set_title(title, fontsize="x-large")
-fig.suptitle("Commensurate/Incommensurate", fontsize="xx-large")
+    ax.set_title(title, fontsize=FONT_SIZE, pad=15)
+
+    for which, spine in ax.spines.items():
+        spine.set_linewidth(SPINE_WIDTH)
+    ax.tick_params(
+        axis="both", which="both", length=TICK_LENGTH, width=TICK_WIDTH,
+        labelsize=LABEL_SIZE,
+    )
+
+# fig.suptitle("Commensurate/Incommensurate", fontsize=FONT_SIZE)
 plt.show()
-fig.savefig("demo/ShowCommensurate.jpg", dpi=1000)
+fig.savefig("demo/ShowCommensurate.jpg", dpi=FIG_DPI)
 plt.close("all")
