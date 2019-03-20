586d6927f4e341ed6ee9dee3b01cae1d14f14527
wangshiphys <wangshiphys@gmail.com>
2019-03-20 14:19:35 +0800

    Use default values for figure and axes properties

--- ShowCommensurate.py
+++ ShowCommensurate.py
@@ -7,9 +7,6 @@ system
 import matplotlib.pyplot as plt
 import numpy as np
 
-from TaS2DataBase import LINE_WIDTH, MARKER_SIZE, FONT_SIZE, FIG_DPI
-from TaS2DataBase import SPINE_WIDTH, TICK_WIDTH, TICK_LENGTH, LABEL_SIZE
-
 
 start = 0
 end = 8
@@ -23,19 +20,11 @@ ys2 = np.cos(2 * np.pi * alphas / np.sqrt(3))
 fig, axes = plt.subplots(3, 1, sharex=True)
 titles = [r"$R_2=1$", r"$R_2=3/2$", r"$R_2=\sqrt{3}$"]
 for ax, ys, title in zip(axes, [ys0, ys1, ys2], titles):
-    ax.plot(alphas, ys, lw=LINE_WIDTH)
-    ax.plot(xs, [0] * len(xs), marker="o", ls="", ms=MARKER_SIZE)
+    ax.plot(alphas, ys)
+    ax.plot(xs, [0] * len(xs), marker="o", ls="")
     ax.set_xlim(start, end)
-    ax.set_title(title, fontsize=FONT_SIZE, pad=15)
+    ax.set_title(title)
 
-    for which, spine in ax.spines.items():
-        spine.set_linewidth(SPINE_WIDTH)
-    ax.tick_params(
-        axis="both", which="both", length=TICK_LENGTH, width=TICK_WIDTH,
-        labelsize=LABEL_SIZE,
-    )
-
-# fig.suptitle("Commensurate/Incommensurate", fontsize=FONT_SIZE)
 plt.show()
-fig.savefig("demo/ShowCommensurate.jpg", dpi=FIG_DPI)
+fig.savefig("demo/ShowCommensurate.jpg")
 plt.close("all")
