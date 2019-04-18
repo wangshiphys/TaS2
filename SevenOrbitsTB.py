a98a8d051bcb4a50cd5771527719b3132530bf9d
wangshiphys <wangshiphys@gmail.com>
2019-04-18 10:32:25 +0800

    Add global variables that describe the hoppings between different orbits; Change the internal implementation of constructing the model Hamiltonian in k-space.

--- SevenOrbitsTB.py
+++ SevenOrbitsTB.py
@@ -16,6 +16,22 @@ from TaS2DataBase import *
 color_map = plt.get_cmap("tab10")
 COLORS = color_map(range(color_map.N))
 
+ONSITE_TERMS = [
+    [(0, 0)],
+    [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)],
+    [(1, 4), (2, 5), (3, 6)],
+    [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1)],
+    [(1, 3), (2, 4), (3, 5), (4, 6), (5, 1), (6, 2)],
+]
+INTER_TERMS_A = [(3, 6), (1, 4), (3, 1), (4, 6)]
+INTER_TERMS_B = [(2, 5), (6, 3), (2, 6), (3, 5)]
+INTER_TERMS_C = [(1, 4), (5, 2), (1, 5), (2, 4)]
+INTER_TERMS = {
+    0: INTER_TERMS_B,
+    1: INTER_TERMS_C,
+    2: INTER_TERMS_A,
+}
+
 
 class SevenOrbitTBSolver(BaseTBSolver):
     """
@@ -47,38 +63,29 @@ class SevenOrbitTBSolver(BaseTBSolver):
     # Construct the Seven-Orbits Tight-Binding model Hamiltonian in the k-space
     # Diagonalize the model Hamiltonian
     def _EnergyCore(self, exponents, *, return_vectors=True, **model_params):
-        defaults = SevenOrbitTBSolver.default_model_params
-        mu_c = model_params.get("mu_c", defaults["mu_c"])
-        t_sc = model_params.get("t_sc", defaults["t_sc"])
-        t_ss1 = model_params.get("t_ss1", defaults["t_ss1"])
-        t_ss2 = model_params.get("t_ss2", defaults["t_ss2"])
-        t_ss3 = model_params.get("t_ss3", defaults["t_ss3"])
-        t_ss4 = model_params.get("t_ss4", defaults["t_ss4"])
-        t_ss5 = model_params.get("t_ss5", defaults["t_ss5"])
-        t_ss6 = model_params.get("t_ss6", defaults["t_ss6"])
+        new_model_params = dict(SevenOrbitTBSolver.default_model_params)
+        new_model_params.update(model_params)
+        onsite_term_coeffs = [
+            new_model_params["mu_c"] / 2, new_model_params["t_sc"],
+            new_model_params["t_ss1"], new_model_params["t_ss3"],
+            new_model_params["t_ss5"],
+        ]
+        inter_term_coeffs = [
+            new_model_params["t_ss2"], new_model_params["t_ss4"],
+            new_model_params["t_ss6"], new_model_params["t_ss6"],
+        ]
 
         shape = (exponents.shape[0], self._orbit_num, self._orbit_num)
         HM = np.zeros(shape, dtype=np.complex128)
-        HM[:, 0, 0] += mu_c / 2
-        HM[:, 0, 1:] += t_sc
-        for i, j in [(1, 4), (2, 5), (3, 6)]:
-            HM[:, i, j] += t_ss1
-        for i, j in [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1)]:
-            HM[:, i, j] += t_ss3
-        for i, j in [(1, 3), (2, 4), (3, 5), (4, 6), (5, 1), (6, 2)]:
-            HM[:, i, j] += t_ss5
 
-        amplitudes = [t_ss2, t_ss4, t_ss4, t_ss4, t_ss6, t_ss6]
-        bonds = [(3, 6), (1, 1), (1, 4), (4, 4), (3, 1), (4, 6)]
-        for t, (i, j) in zip(amplitudes, bonds):
-            HM[:, i, j] += t * exponents[:, 0]
-        bonds = [(2, 5), (6, 6), (6, 3), (3, 3), (2, 6), (3, 5)]
-        for t, (i, j) in zip(amplitudes, bonds):
-            HM[:, i, j] += t * exponents[:, 1]
-        bonds = [(1, 4), (5, 5), (5, 2), (2, 2), (1, 5), (2, 4)]
-        for t, (i, j) in zip(amplitudes, bonds):
-            HM[:, i, j] += t * exponents[:, 2]
+        for coeff, terms in zip(onsite_term_coeffs, ONSITE_TERMS):
+            for row, col in terms:
+                HM[:, row, col] += coeff
+        for index, terms in INTER_TERMS.items():
+            for coeff, (row, col) in zip(inter_term_coeffs, terms):
+                HM[:, row, col] += coeff * exponents[:, index]
         HM += np.transpose(HM.conj(), axes=(0, 2, 1))
+
         if return_vectors:
             return np.linalg.eigh(HM)
         else:
@@ -181,7 +188,7 @@ if __name__ == "__main__":
         "t_ss6": 0.042,
     }
 
-    Solver = SevenOrbitTBSolver(numkx=100)
+    Solver = SevenOrbitTBSolver(e_num=13, numkx=200, numky=200)
     Solver.VisualizeDOS(**model_params)
     Solver()
     Solver(**model_params)
