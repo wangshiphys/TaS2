07c3472d651367be2fd4b06d13fd9ad3bc91e63b
wangshiphys <wangshiphys@gmail.com>
2021-01-26 11:21:18 +0800

    Add `PlotDOS.py` to `MyStarOfDavidModel` sub-package.

---
+++ MyStarOfDavidModel/PlotDOS.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+eta = 0.10
+data_path = "data/DOS/eta={0:.2f}/".format(eta)
+data_name_template = "DOS for cluster=StarOfDavid,enum={0},{1}.npz"
+
+fig_path = "fig/DOS/eta={0:.2f}/".format(eta)
+Path(fig_path).mkdir(exist_ok=True, parents=True)
+fig_name_template = "DOS for cluster=StarOfDavid,enum={0},{1}.jpg"
+
+title_template = r"$N={N},\mu_0={mu0: .2f},\mu_1={mu1: .2f},\mu_2={mu2: .2f}," \
+                 r"t_0={t0: .2f},t_1={t1: .2f},t_2={t2: .2f}," \
+                 r"t_3={t3: .2f},t_4={t4: .2f},t_5={t5: .2f},U={U: .2f}$"
+
+default_model_parameters = {
+    "t0" : -0.2,
+    "t1" : -0.2,
+    "t2" : -0.8,
+    "t3" : -1.0,
+    "t4" : -1.0,
+    "t5" : -1.0,
+    "mu0":  0.4,
+    "mu1": -0.2,
+    "mu2":  0.1,
+    "U"  :  0.0,
+}
+
+e_nums = [13, 14]
+Us = np.arange(0.0, 2.1, 0.1)
+for U in Us:
+    new_model_params = dict(default_model_parameters)
+    new_model_params.update(U=U)
+    tmp = ",".join(
+        "{0}={1:.2f}".format(key, value)
+        for key, value in new_model_params.items()
+    )
+    for e_num in e_nums:
+        data_name = data_name_template.format(e_num, tmp)
+        fig_name = fig_name_template.format(e_num, tmp)
+        try:
+            with np.load(data_path + data_name) as data:
+                omegas = data["omegas"]
+                dos = data["dos"]
+        except OSError:
+            continue
+
+        fig, ax = plt.subplots()
+        ax.plot(omegas, dos)
+        ax.set_title(
+            title_template.format(N=e_num, **new_model_params),
+            fontsize="xx-large"
+        )
+        plt.get_current_fig_manager().window.showMaximized()
+        plt.tight_layout()
+        plt.show()
+        fig.savefig(fig_path + fig_name, dpi=300)
+        plt.close("all")
